815b3d3a006480b56e5f1031bbd8b11a904ddf5e
YuHongHerald <yuhongherald@gmail.com>
2017-05-29 16:43:59 +0800

    Passing code around.

---
+++ app/src/main/java/orbital/dingletutors/Layouts.java
@@ -0,0 +1,17 @@
+package orbital.dingletutors;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Herald on 27/5/2017.
+ */
+
+public class Layouts {
+    public static RelativeLayout viewLesson(Context context) {
+        RelativeLayout layout = (RelativeLayout) ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                .inflate(R.layout.view_lesson, null);
+        return layout;
+    }
+}
